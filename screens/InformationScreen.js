f736cd1cf3b7db00260107118f8bf4a946917eca
AyaLov <88025339+AyaLov@users.noreply.github.com>
2023-06-28 21:39:49 +0900

    dd
    dd

--- screens/InformationScreen.js
+++ screens/InformationScreen.js
@@ -2,11 +2,11 @@
 import { View, Text } from "react-native";
 
 function InfromationScreen({route}) {
-    const pressedExpenseId = route.params?.exhibitionId; // 메인화면에서 클릭된 exhibition id 가져옴
+    const pressedExhibitionId = route.params?.exhibitionId; // 메인화면에서 클릭된 exhibition id 가져옴
     return (
         <View>
             <Text>전시회 상세 정보 페이지</Text>
-            <Text>{pressedExpenseId}</Text>
+            <Text>{pressedExhibitionId}</Text>
         </View>
     );
 }
