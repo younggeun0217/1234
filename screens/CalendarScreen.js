564036d842ff84e333c4e0e767e6384707156c35
AyaLov <88025339+AyaLov@users.noreply.github.com>
2023-07-14 17:37:14 +0900

    임시 캘린더
    임시

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,7 +1,52 @@
-import { Text } from "react-native";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
 function CalendarScreen() {
-    return <Text>캘린더 페이지 입니다.</Text>;
+  const getMarked = (schedules) => {
+    let marked = {};
+    schedules.forEach((schedule) => {
+      const { startingDay, duration, color } = schedule; // Destructure the text from the schedule
+      for (let i = 0; i < duration; i++) {
+        let day = new Date(startingDay);
+        day.setDate(day.getDate() + i);
+        let formattedDay = day.toISOString().split('T')[0];
+        let periods = [
+          {
+            startingDay: i === 0,
+            endingDay: i === duration - 1,
+            color, // Use the color provided in the schedule
+          },
+        ];
+        if (marked[formattedDay]) {
+          marked[formattedDay].periods.push(...periods);
+        } else {
+          marked[formattedDay] = { periods };
+        }
+      }
+    });
+    return marked;
+  };
+
+  return (
+    <View style={styles.root}>
+      <Calendar
+        markingType="multi-period"
+        markedDates={getMarked([
+          { startingDay: '2023-05-05', duration: 62, color: 'red'},
+          { startingDay: '2023-06-20', duration: 10, color: 'blue'},
+          // Add more schedules here
+        ])}
+      />
+    </View>
+  );
 }
 
 export default CalendarScreen;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    marginTop: 150,
+  },
+});
